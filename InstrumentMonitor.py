927437b085b3d491a871f558587c09ff871a0f18
doireannocarroll <146376354+doireannocarroll@users.noreply.github.com>
2026-05-19 21:45:52 +0100

    Add files via upload

--- InstrumentMonitor.py
+++ InstrumentMonitor.py
@@ -1,13 +1,13 @@
-#imports
-
+#imports and installs
 import time
+import adafruit_dht
 from sense_hat import SenseHat
-
+sense=SenseHat()
 
 
 #THINGSPEAK INFO - TO BE INCLUDED LATER
 #THINGSPEAK_API_KEY = 'V2NZHXW8GI9T6ZKX'
-#TO_EMAIL = 'xxxxxxxxxxxx@gmail.com'
+#TO_EMAIL = 'doireannocarroll1998@gmail.com'
 
 #Colours for SenseHat
 BLUE = (0, 0, 255)
@@ -24,11 +24,11 @@ def get_environmental_data():
 
 while True:
     # Get readings
-    temp, humidity, pressure = get_environmental_data()
+    temp, humidity= get_environmental_data()
     
     # Print to command line
-    print("Temp: {:.1f} C  Humidity: {:.1f}%  Pressure: {:.1f} hPa".format(
-        temp, humidity, pressure))
+    print("Temp: {:.1f} C  Humidity: {:.1f}%".format(
+        temp, humidity))
     
     # Display on LED matrix (one after the other)
     sense.show_message("T:{:.1f}C".format(temp), text_colour=RED)
